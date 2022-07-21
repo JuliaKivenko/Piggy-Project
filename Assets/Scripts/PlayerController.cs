8a89e9870911eb6312be0839f835938713613445
JuliaKivenko <kivenko.julia@yahoo.com>
2022-07-21 13:58:51 +0200

    PlayerController
    Implementing player controls and camera follow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerControllerAction playerControllerAcion;
+    [SerializeField] CharacterController characterController;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float dashSpeedMultiplier;
+
+    private float dashSpeed;
+    [SerializeField] private float currentSpeed;
+
+    private void Awake()
     {
-        
+        playerControllerAcion = new PlayerControllerAction();
+        dashSpeed = walkSpeed * dashSpeedMultiplier;
+        currentSpeed = walkSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        playerControllerAcion.Enable();
+        playerControllerAcion.Player.Dash.performed += Dash;
+    }
+
+    private void OnDisable()
+    {
+        playerControllerAcion.Disable();
+        playerControllerAcion.Player.Dash.performed -= Dash;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 movemementInput = playerControllerAcion.Player.Move.ReadValue<Vector2>();
+        Vector3 toConvert = new Vector3(movemementInput.x, 0, movemementInput.y);
+        Vector3 move = IsoVectorConvert(toConvert);
+        characterController.Move(move * Time.deltaTime * currentSpeed);
+    }
+
+    public void Dash(InputAction.CallbackContext context)
+    {
+        currentSpeed = dashSpeed;
+        if (context.canceled)
+            currentSpeed = walkSpeed;
+    }
+
+
+    private Vector3 IsoVectorConvert(Vector3 vector)
+    {
+        Quaternion rotaton = Quaternion.Euler(0, 45, 0);
+        Matrix4x4 isoMatrix = Matrix4x4.Rotate(rotaton);
+        Vector3 result = isoMatrix.MultiplyPoint3x4(vector);
+        return result;
     }
 }
---
+++ Assets/Scripts/PlayerControllerAction.cs
@@ -0,0 +1,161 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControllerAction.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControllerAction : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControllerAction()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControllerAction"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""da4275ce-fcc1-42e2-942f-f262fedc6d62"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""aa9b7373-4e0a-4ac0-a771-395461c4d062"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""5fc6db68-cf38-47a2-bfa1-d35bc0a27af2"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""8148bc0a-dbbe-4d5e-a3f6-2494d3478cd8"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d43b5b69-170a-4c35-a514-8dad8986fc09"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": ""Press,Press(behavior=1)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Dash;
+    public struct PlayerActions
+    {
+        private @PlayerControllerAction m_Wrapper;
+        public PlayerActions(@PlayerControllerAction wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Dash.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
+    }
+}
